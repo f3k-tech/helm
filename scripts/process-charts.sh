dd962afb07776870357d8a24e2988c788dd39240
FreekO <freek@olgers.com>
2023-11-15 21:09:35 +0100

    fix chart dir

--- scripts/process-charts.sh
+++ scripts/process-charts.sh
@@ -4,10 +4,10 @@
 for d in */ ; do
   chart_name=$(basename "$d")
   chart_version=$(yq e '.version' "$d/Chart.yaml")
-  image_tag=$(yq e '.image.tag' "$chart_dir/values.yaml")
+  image_tag=$(yq e '.image.tag' "$d/values.yaml")
 
   # Update the image.tag in values.yaml using yq
-  yq e -i '.appVersion = strenv(image_tag)' "$chart_dir/Chart.yaml" image_tag="$image_tag"
+  yq e -i '.appVersion = strenv(image_tag)' "$d/Chart.yaml" image_tag="$image_tag"
 
   if [ ! -f "../$chart_name-$chart_version.tgz" ]; then
     helm package --sign --key info@f3k.tech --keyring ~/.gnupg/secring.gpg "$d" -d ..
