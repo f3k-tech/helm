cc198782d7763e5f6aa3050a5de442f84724f0d9
FreekO <freek@olgers.com>
2023-11-15 10:38:03 +0300

    make scripts executable

--- scripts/process-charts.sh
+++ scripts/process-charts.sh
@@ -4,10 +4,7 @@
 for d in */ ; do
   chart_name=$(basename "$d")
   chart_version=$(yq e '.version' "$d/Chart.yaml")
-  app_version=$(yq e '.appVersion' "$d/Chart.yaml")
-
-  # Update the image.tag in values.yaml using yq
-  yq e -i '.image.tag = strenv(app_version)' "$d/values.yaml" app_version="$app_version"
+  ./update-chart-version.sh
 
   if [ ! -f "../$chart_name-$chart_version.tgz" ]; then
     helm package --sign --key info@f3k.tech --keyring ~/.gnupg/secring.gpg "$d" -d ..
