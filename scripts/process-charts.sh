853910fb8aec5849e6ebbc075d7b2e1677d0697d
FreekO <freek@olgers.com>
2023-11-15 13:47:25 +0300

    update script to use image_tag to update appversion

--- scripts/process-charts.sh
+++ scripts/process-charts.sh
@@ -4,7 +4,10 @@
 for d in */ ; do
   chart_name=$(basename "$d")
   chart_version=$(yq e '.version' "$d/Chart.yaml")
-  ../scripts/update-chart-version.sh
+  image_tag=$(yq e '.image.tag' "$chart_dir/values.yaml")
+
+  # Update the image.tag in values.yaml using yq
+  yq e -i '.appVersion = strenv(image_tag)' "$chart_dir/Chart.yaml" image_tag="$image_tag"
 
   if [ ! -f "../$chart_name-$chart_version.tgz" ]; then
     helm package --sign --key info@f3k.tech --keyring ~/.gnupg/secring.gpg "$d" -d ..
