1d9a5f90e64e79aae1f232fa15e78ab731429c23
FreekO <info@f3k.tech>
2023-12-13 15:32:43 +0300

    Remove obsolete code to copy tag from Chart to values

--- scripts/process-charts.sh
+++ scripts/process-charts.sh
@@ -3,10 +3,6 @@
 # Loop through all chart directories
 for d in */ ; do
   chart_name=$(basename "$d")
-  chart_version=$(yq e '.version' "$d/Chart.yaml")
-
-  image_tag=$(yq e '.image.tag' "$d/values.yaml") yq -i '.appVersion=env(image_tag)' "$d/Chart.yaml"   
-
   if [ ! -f "../$chart_name-$chart_version.tgz" ]; then
     helm package --sign --key info@f3k.tech --keyring ~/.gnupg/secring.gpg "$d" -d ..
   else
